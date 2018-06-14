38944048f4eebf32704a7c02c666686756d4bc83
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-14 22:23:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const myPokerHand = new PokerHand('KS KH QC AH AD')
-const hisPokerHand = new PokerHand('AH KH QH JH TH')
+const myPokerHand = new PokerHand('KS KH QC AH AD');
+const hisPokerHand = new PokerHand('AH KH QH JH TH');
 express()
   
   .use(express.static(path.join(__dirname, 'public')))
