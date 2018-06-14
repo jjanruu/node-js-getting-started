7ac313ec73abde09398903d650773ab9930c441a
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-14 22:33:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const PokerHand = require('poker-hand-evaluator');
-const hisPokerHand = new PokerHand('AH KH QH JH TH');
+const myPokerHand = new PokerHand('KS KH QC AH AD');
 express()
-  
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send(hisPokerHand.describe())
+  .get('/', (req, res) => res.send(myPokerHand.describe()))
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
