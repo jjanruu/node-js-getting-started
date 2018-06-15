e3102590c068635cf3c26737fb7e788965f53564
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-15 19:09:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 var Combinatorics = require('js-combinatorics');
 var cmb, a;
 cmb = Combinatorics.combination(['a','b','c','d'], 2);
-while(a = cmb.next()) res.send(a);
+
 
 const PokerHand = require('poker-hand-evaluator');
 const myPokerHand = new PokerHand('KS KH QC AH AD');
@@ -14,5 +14,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.send(myPokerHand.describe()))
- .get('/Combi', (req, res) => while(a = cmb.next()) res.send(a);
+  .get('/Combi', (req, res) => while(a = cmb.next()) res.send(a);
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
