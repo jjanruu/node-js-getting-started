a60470a624cd00128b60b2f8c9ab519ef15eadfa
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-15 21:08:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var combinatorics = require('js-combinatorics');
-var cmb, a = 'asd';
-cmb = Combinatorics.combination(['a', 'b', 'c', 'd'], 2);
+var Combinatorics = require('js-combinatorics');
+var cmb, a;
+cmb = Combinatorics.combination(['a','b','c','d'], 2);
 
 
 const PokerHand = require('poker-hand-evaluator');
 const myPokerHand = new PokerHand('KS KH QC AH AD');
 express()
-  .use(express.static (path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.send(myPokerHand.describe()))
