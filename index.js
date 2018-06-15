a999813ae5e1d3998916f277f518b37b0a1de33d
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-15 21:17:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) {
-       res.send(a);
-});
+       res.send(a)
+})
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
