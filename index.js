ab3deff2a015a96383e772c1e22850cc542c6619
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-18 19:28:01 +0800

    try

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ express()
 
   .get('/poker/:hand', (req, res) =>
        {
-        res.send(req.params);
+        res.send(req.params.hand);
         cmb = Combinatorics.combination([":hand"], 5);
         var combi = [];
         while(a = cmb.next())
