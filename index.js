129771d9b3625399e77a9df815d397a3bf791d19
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-19 16:45:14 +0800

    asd

--- index.js
+++ index.js
@@ -22,20 +22,25 @@ express()
         var cmb = Combinatorics.combination(cnv, 5);
         var combi = [];
         var combToString = [];
-        var i = 0;
+        //var i = 0;
         //const myPokerHand = new PokerHand();
-        while(a = cmb.next())
+        /*while(a = cmb.next())
         {
           combi.push(a);
           //combToString[i].push(combi[i].join().replace(/\,/ig, " "));
           //myPokerHand = new PokerHand(combToString[a]);
           combToString[i].push(combi[i].join().replace(/\,/ig, " "));
           i++;
+        }*/
+
+        for(var i = 0; i < combi.length; i++)
+        {
+          combi.push(a);
         }
 
         //var combToString = combi[0].join().replace(/\,/ig, " "); //join = tostring() // replacing "," to " " and i = ignore case sensitive, g = global
         //const myPokerHand = new PokerHand(combToString);
-        res.send("hello");
+        res.send(combi);
 //var id = req.params.id
   })
        
