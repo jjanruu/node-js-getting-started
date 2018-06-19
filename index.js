873183a7c56f3c915db96796d19900713e11c5d2
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-19 14:43:52 +0800

    try

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 
 var Combinatorics = require('js-combinatorics');
 var a;
-var myPokerHand = [];
+var myPokerHand;
 const PokerHand = require('poker-hand-evaluator');
 
 express()
@@ -27,7 +27,7 @@ express()
         {
           combi.push(a);
         }
-        myPokerHand[0] = new PokerHand(combi[0]);
+        myPokerHand = new PokerHand(combi[0]);
         res.send(myPokerHand);
 //var id = req.params.id
   })
