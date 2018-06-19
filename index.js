3d7e9058e51e120d29f29ea66df1e2b7d7a280fc
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-19 16:17:56 +0800

    asd

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ express()
         {
           combi.push(a);
           //combToString[i].push(combi[i].join().replace(/\,/ig, " "));
-          i++;
+          i = i + 1;
           //myPokerHand = new PokerHand(combToString[a]);
         }
         //var combToString = combi[0].join().replace(/\,/ig, " "); //join = tostring() // replacing "," to " " and i = ignore case sensitive, g = global
