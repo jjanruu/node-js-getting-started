8c23ce5d1500f2e07ee2fca4b02c10b6642265be
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-19 16:41:04 +0800

    try
    try

--- index.js
+++ index.js
@@ -22,18 +22,17 @@ express()
         var cmb = Combinatorics.combination(cnv, 5);
         var combi = [];
         var combToString = [];
+        var i = 0;
         //const myPokerHand = new PokerHand();
         while(a = cmb.next())
         {
           combi.push(a);
           //combToString[i].push(combi[i].join().replace(/\,/ig, " "));
           //myPokerHand = new PokerHand(combToString[a]);
+          combToString[i].push(combi[i].join().replace(/\,/ig, " "));
+          i++;
         }
 
-        for (var i = 0; i < combi.length; i++)
-        {
-          //combToString[i].push(combi[i].join().replace(/\,/ig, " "));
-        }
         //var combToString = combi[0].join().replace(/\,/ig, " "); //join = tostring() // replacing "," to " " and i = ignore case sensitive, g = global
         //const myPokerHand = new PokerHand(combToString);
         res.send("hello");
