204c4d2ce1cdc7e75ccb35deeb390bb6efe6bb74
John Rudolf Cadag <janru.cadag@gmail.com>
2018-06-19 23:01:42 +0800

    try

--- index.js
+++ index.js
@@ -37,8 +37,12 @@ express()
         {
           combToString.push(new PokerHand(combi[i].join().replace(/\,/ig, " ")));
         }
-
-        res.send(combToString);
+        var months = [{"hand":["TS","JS","QS","KS","AS"],"score":1,"rank":"ROYAL_FLUSH"},{"hand":["TS","JS","QS","KS","5H"],"score":6682,"rank":"HIGH_CARD"},{"hand":["TS","JS","QS","AS","5H"],"score":6354,"rank":"HIGH_CARD"},{"hand":["TS","JS","KS","AS","5H"],"score":6234,"rank":"HIGH_CARD"},{"hand":["TS","QS","KS","AS","5H"],"score":6198,"rank":"HIGH_CARD"},{"hand":["JS","QS","KS","AS","5H"],"score":6190,"rank":"HIGH_CARD"},{"hand":["TS","JS","QS","KS","QC"],"score":3821,"rank":"ONE_PAIR"},{"hand":["TS","JS","QS","AS","QC"],"score":3776,"rank":"ONE_PAIR"},{"hand":["TS","JS","KS","AS","QC"],"score":1600,"rank":"STRAIGHT"},{"hand":["TS","QS","KS","AS","QC"],"score":3767,"rank":"ONE_PAIR"},{"hand":["JS","QS","KS","AS","QC"],"score":3766,"rank":"ONE_PAIR"},{"hand":["TS","JS","QS","5H","QC"],"score":3870,"rank":"ONE_PAIR"},{"hand":["TS","JS","KS","5H","QC"],"score":6682,"rank":"HIGH_CARD"},{"hand":["TS","QS","KS","5H","QC"],"score":3834,"rank":"ONE_PAIR"},{"hand":["JS","QS","KS","5H","QC"],"score":3826,"rank":"ONE_PAIR"},{"hand":["TS","JS","AS","5H","QC"],"score":6354,"rank":"HIGH_CARD"},{"hand":["TS","QS","AS","5H","QC"],"score":3789,"rank":"ONE_PAIR"},{"hand":["JS","QS","AS","5H","QC"],"score":3781,"rank":"ONE_PAIR"},{"hand":["TS","KS","AS","5H","QC"],"score":6198,"rank":"HIGH_CARD"},{"hand":["JS","KS","AS","5H","QC"],"score":6190,"rank":"HIGH_CARD"},{"hand":["QS","KS","AS","5H","QC"],"score":3772,"rank":"ONE_PAIR"}];
+months.sort(function(obj1, obj2) {
+  // Ascending: first age less than the previous
+        res.send(obj1.score - obj2.score);
+});
+        //res.send(combToString);
   })
        
   
